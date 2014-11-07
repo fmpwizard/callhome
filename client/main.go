5556d7f761a5c2365006d909cd5126a2c0dbf8ad
Diego Medina <diego@fmpwizard.com>
2014-11-06 22:10:28 -0500

    Basic client code in palce, fetches new messages from the server on this same project
    in a loop, once every 2 seconds for now

--- client/main.go
+++ client/main.go
@@ -2,7 +2,6 @@ package main
 
 //client
 import (
-	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -11,18 +10,15 @@ import (
 
 func main() {
 	for {
-		var message RoomMessage
-		json.Unmarshal(fetchRoomMessage("stella"), &message)
-		log.Println(message.Node.Value)
-		//log.Println(string(fetchRoomMessage("stella")))
-		time.Sleep(10 * time.Millisecond)
+		log.Printf("Message: %s", string(fetchRoomMessage("Stella")))
+		time.Sleep(2000 * time.Millisecond)
 	}
 }
 
 func fetchRoomMessage(room string) []byte {
-	ret, err := http.Get("http://127.0.0.1:8080/read?room=" + room)
+	ret, err := http.Get("http://127.0.0.1:7070/read?room=" + room)
 	if err != nil {
-		log.Printf("ERROR: Could not get message for key: %s, we got error: %s", room, err.Error())
+		log.Printf("ERROR: Could not get message for room: %s, we got error: %s", room, err.Error())
 		return nil
 	}
 	defer ret.Body.Close()
@@ -32,16 +28,4 @@ func fetchRoomMessage(room string) []byte {
 		return nil
 	}
 	return body
-}
-
-type RoomMessage struct {
-	Action string
-	Node   Etcdnode
-}
-
-type Etcdnode struct {
-	Key           string
-	Value         string
-	ModifiedIndex int
-	CreatedIndex  int
 }
