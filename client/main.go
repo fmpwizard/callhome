3c6e58b99085658a40854b34f855c947be3d7dec
Diego Medina <diego@fmpwizard.com>
2014-11-07 22:45:30 -0500

    Simply use /index to handle the initial page and the sending of messages (POST request) Added a /crash page to force an app crash to test out failover Added error logging to template processing Use variables to enter which rooms are displayed on the UI Handle multiple rooms selections

--- client/main.go
+++ client/main.go
@@ -2,27 +2,44 @@ package main
 
 //client
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os/exec"
+	"strings"
 	"time"
 )
 
+//define server url  and port
+// define room to listen to
+
 func main() {
+
+	var host string
+	var port string
+	var room string
+	flag.StringVar(&port, "port", "7070", "Server port number to connect to")
+	flag.StringVar(&host, "host", "127.0.0.1", "Server host (ip or domain) to connect to")
+	flag.StringVar(&room, "room", "All", "The room name you are in")
+	flag.Parse()
+
 	for {
-		msg := string(fetchRoomMessage("Stella"))
-		log.Printf("Message: %s", msg)
-		err := exec.Command("espeak", "-ven+f3", "-k5", "-s120", msg).Run()
-		if err != nil {
-			log.Printf("ERROR: Could not read the message %s", err.Error())
+		msg := string(fetchRoomMessage(room))
+		if len(strings.TrimSpace(msg)) > 0 {
+			log.Printf("Message: %s", msg)
+			err := exec.Command("espeak", "-ven+f3", "-k5", "-s120", msg).Run()
+			if err != nil {
+				log.Printf("ERROR: Could not read the message %s", err.Error())
+			}
 		}
 		time.Sleep(2000 * time.Millisecond)
 	}
 }
 
 func fetchRoomMessage(room string) []byte {
-	ret, err := http.Get("http://127.0.0.1:7070/read?room=" + room)
+	serverUrl := fmt.Sprintf("http://%s:%s", host, port)
+	ret, err := http.Get(serverUrl + "/read?room=" + room)
 	if err != nil {
 		log.Printf("ERROR: Could not get message for room: %s, we got error: %s", room, err.Error())
 		return nil
--- server/main.go
+++ server/main.go
@@ -2,56 +2,70 @@ package main
 
 //server
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"github.com/iron-io/iron_go/mq"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 )
 
+//define port to listen to
+
+var rooms = []string{"Stella", "Diego", "Hayley", "Gabriel"}
+
+type templateData struct {
+	Rooms []string
+	Msg   string
+}
+
 func main() {
-	http.HandleFunc("/index", index)
+	var port string
+	fs := http.FileServer(http.Dir("static"))
+	flag.StringVar(&port, "port", "7070", "port number to bind to")
+	flag.Parse()
+	http.HandleFunc("/", index)
 	http.HandleFunc("/read", read)
-	http.HandleFunc("/send", sendMessage)
+	http.HandleFunc("/crash", crash)
 
 	http.Handle("/js/", http.StripPrefix("/js/", fs))
 	http.Handle("/css/", http.StripPrefix("/css/", fs))
-
-	//http.HandleFunc("/ack", ackMessage)
 	log.Println("Starting server ...")
-
-	log.Fatal(http.ListenAndServe(":7070", nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 func index(rw http.ResponseWriter, req *http.Request) {
+	if req.Method == "POST" {
+		sendMessage(rw, req)
+	} else {
+		serveIndex(rw, req)
+	}
+}
+
+func serveIndex(rw http.ResponseWriter, req *http.Request) {
 	tmpl, err := template.ParseFiles("index.html")
 	if err != nil {
 		http.Error(rw, "Could not parse html template, ERROR: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
-	tmpl.ExecuteTemplate(rw, "index.html", nil)
+	err = tmpl.ExecuteTemplate(rw, "index.html", templateData{Rooms: rooms, Msg: ""})
+	if err != nil {
+		log.Printf("ERROR: template gave %s", err.Error())
+	}
 }
-
-var fs = http.FileServer(http.Dir("static"))
 
 func sendMessage(rw http.ResponseWriter, req *http.Request) {
-	room := req.FormValue("room")
-	if strings.TrimSpace(room) == "" {
-		http.Error(rw, "Missing room name", http.StatusInternalServerError)
-		return
-	}
-	queue := mq.New(room)
-	message := req.FormValue("message")
-	if strings.TrimSpace(message) == "" {
-		http.Error(rw, "Missing message text", http.StatusInternalServerError)
-		return
-	}
-	_, err := queue.PushString(message)
-	if err != nil {
-		log.Printf("ERROR: Could not send message to queue: %v", err)
-		http.Error(rw, "Could not send message to queue", http.StatusInternalServerError)
-		return
+	msg := req.FormValue("message")
+	_ = req.ParseForm()
+	for _, r := range req.Form["room"] {
+		err := sendToQueue(r, msg)
+		if err != nil {
+			http.Error(rw, "Missing message text", http.StatusInternalServerError)
+			return
+		}
 	}
 
 	tmpl, err := template.ParseFiles("index.html")
@@ -59,10 +73,31 @@ func sendMessage(rw http.ResponseWriter, req *http.Request) {
 		http.Error(rw, "Could not parse html template, ERROR: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
-	tmpl.ExecuteTemplate(rw, "index.html", "Message was sent.")
+	tmpl.ExecuteTemplate(rw, "index.html", templateData{Rooms: rooms, Msg: "Message was sent."})
 
 }
 
+func sendToQueue(room, msg string) error {
+	if strings.TrimSpace(room) == "" {
+		return errors.New("Missing room name")
+	}
+	queue := mq.New(room)
+	if strings.TrimSpace(msg) == "" {
+		return errors.New("Missing message text")
+	}
+	_, err := queue.PushString(msg)
+	if err != nil {
+		log.Printf("ERROR: Could not send message to queue: %v", err)
+		return errors.New("Could not send message to queue")
+	}
+	return nil
+
+}
+
+/*func pushMessageToAll(msg string) {
+	queue := mq.New(queueName)
+}
+*/
 func read(rw http.ResponseWriter, req *http.Request) {
 	room := req.FormValue("room")
 	if strings.TrimSpace(room) == "" {
@@ -83,4 +118,9 @@ func read(rw http.ResponseWriter, req *http.Request) {
 	} else {
 		rw.WriteHeader(http.StatusNotModified)
 	}
+}
+
+func crash(rw http.ResponseWriter, req *http.Request) {
+	log.Println("Simulating a crash, goodbye!")
+	os.Exit(1)
 }
